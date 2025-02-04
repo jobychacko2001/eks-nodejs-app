92bc5f04520d41e18a8d642e0886b27f1c96ef15
Joby Chacko <jobychacko2001@gmail.com>
2025-02-04 15:26:29 -0500

    Create variables.tf

---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
